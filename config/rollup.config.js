952cb4abb64681a480512878341f26f147146f01
nikni <nicola.tommasi.91@gmail.com>
2018-10-28 22:48:00 +0100

    Adding rollup to package the library

---
+++ config/rollup.config.js
@@ -0,0 +1,19 @@
+import vue from 'rollup-plugin-vue'; // Handle .vue SFC files
+import buble from 'rollup-plugin-buble'; // Transpile/polyfill with reasonable browser support
+import uglify from 'rollup-plugin-uglify';
+
+export default {
+    input: 'src/main.ts', // Path relative to package.json
+    output: {
+        name: 'VueVoice',
+        exports: 'named',
+    },
+    plugins: [
+        vue({
+            css: true, // Dynamically inject css as a <style> tag
+            compileTemplate: true, // Explicitly convert template to render function
+        }),
+        buble(), // Transpile to ES5
+        uglify()
+    ],
+};
