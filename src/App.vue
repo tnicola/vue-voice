f7489f9b9d9f046fe4f9cef99da5820c1162cc5f
nikni <nicola.tommasi.91@gmail.com>
2018-10-30 23:42:48 +0100

    Restarting with a clean project

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-    <CustomButton />
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue';
+<script>
 import HelloWorld from './components/HelloWorld.vue';
-// import CustomButton from 'vue-voice';
-import CustomButton from 'vue-voice';
-import VueTour from 'vue-tour';
 
-
-export default Vue.extend({
+export default {
   name: 'app',
   components: {
-    HelloWorld, CustomButton
-  }
-});
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
--- tools/copy-package.js
+++
@@ -1,6 +0,0 @@
-const PATH_LIB = "tools/";
-const PATH_DIST = "dist/";
-
-const fs = require('fs');
-let resizable = fs.readFileSync(PATH_LIB + 'package.json').toString();
-fs.writeFileSync(PATH_DIST + 'package.json', resizable);
